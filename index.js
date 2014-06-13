a4c9f67105a3fadf73ac32d2b7dcd2a157fbbcdc
Per Jansson <per.r.jansson@gmail.com>
2014-06-13 11:57:01 +0200

    Getting forecast for hardcoded long/lat.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 var http = require("http");
+var request = require('request');
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Weather forecast world");
-    response.end();
+http.createServer(function (req, resp) {
+
+    var options = {
+        url: 'http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/58.59/lon/16.18/data.json',
+        method: 'GET'
+    }
+
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+
+            resp.writeHead(200, {"Content-Type": "text/plain"});
+            resp.write(body);
+            resp.end();
+        }
+    })
+
 }).listen(8888);
