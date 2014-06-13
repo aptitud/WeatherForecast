3ca160a51f099116629eb3207aeb594dafd8be66
Per Jansson <per.r.jansson@gmail.com>
2014-06-13 16:34:48 +0200

    Build a rest api to get forecast per latitude and longitude.

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 var http = require("http");
 var request = require('request');
+var express = require('express');
 
-http.createServer(function (req, resp) {
+var app = express();
 
+app.get('/', function (req, res) {
+    res.send('Yes, Weather Forecast (Anders edition) is up and running');
+});
+
+app.get('/forecast/lat/:lat/lon/:lon', function (req, res) {
+    getWeatherForecast(req.params.lat, req.params.lon, function (timeseries, errorMessage) {
+        if (errorMessage) {
+            res.send('Sorry, SMHI says: "' + errorMessage + '"');
+        } else {
+            var timeSeriesArray = toArray(timeseries);
+            res.send(timeSeriesArray);
+        }
+    });
+});
+
+app.listen(8888);
+
+function getWeatherForecast(lat, lon, callback) {
     var options = {
-        url: 'http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/58.59/lon/16.18/data.json',
+        url: 'http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/' + lat + '/lon/' + lon + '/data.json',
         method: 'GET'
     }
 
     request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body)
-
-            resp.writeHead(200, {"Content-Type": "text/plain"});
-            resp.write(body);
-            resp.end();
+        if (response.statusCode == 200) {
+            var forecasts = JSON.parse(body);
+            var timeseries = forecasts.timeseries;
+            return callback(timeseries, null);
+        } else {
+            return callback(null, body);
         }
     })
+}
 
-}).listen(8888);
+function toArray(timeseries) {
+    var timeSeriesArray = [];
+    timeseries.forEach(function (forecast) {
+        timeSeriesArray.push(forecast);
+    });
+    return timeSeriesArray;
+}
