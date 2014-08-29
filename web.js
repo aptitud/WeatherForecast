4be2ec95724f00ee1e9d9cc674b0d09bce6d4c2c
Per Jansson <per.r.jansson@gmail.com>
2014-08-29 11:10:25 +0200

    Web scraping sea weather forecast from SMHI and sending back in response.

--- web.js
+++ web.js
@@ -2,11 +2,20 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var app     = express();
+var app = express();
 
-app.get('/NorraOstersjon', function(req, res) {
-    // Web scrape and tweet
-    res.send('Web scrape and tweet');
+app.get('/SWF', function (req, res) {
+    request('http://www.smhi.se/vadret/hav-och-kust/sjovader/sjovader_tabell_sv.htm', function (error, response, html) {
+        if (!error) {
+            var forecasts = [];
+            var $ = cheerio.load(html);
+            $('table .row').each(function () {
+                var forecast = $(this).text();
+                forecasts.push(forecast);
+            });
+            res.send(forecasts);
+        }
+    })
 });
 
 app.listen(8080)
