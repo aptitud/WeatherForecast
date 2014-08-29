0041e7a2bcafdcfef511c8f005e0be1c9758b585
Per Jansson <per.r.jansson@gmail.com>
2014-08-29 15:02:50 +0200

    Trying to parse out the forecast for Norra Östersjön

--- web.js
+++ web.js
@@ -4,22 +4,35 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
+const FLADEN_OCH_DOGGER = "Fladen och Dogger";
+const NORRA_OSTERSJON = "Norra Östersjön";
+
 app.get('/SWF', function (req, res) {
     request('http://www.smhi.se/vadret/hav-och-kust/sjovader/sjovader_tabell_sv.htm', function (error, response, html) {
         if (!error) {
-            var forecasts = [];
+            var responseText;
             var $ = cheerio.load(html);
             $('table .row').each(function () {
                 var forecast = $(this).text();
-                forecasts.push(forecast);
+                if (forecast.indexOf(NORRA_OSTERSJON) > -1) {
+                    var forecastText = forecast.replace(NORRA_OSTERSJON, '');
+                    responseText = createResponse(NORRA_OSTERSJON, forecastText, res);
+                    sendTweet(NORRA_OSTERSJON, forecastText);
+                }
             });
-            res.send(forecasts);
+            res.send(responseText);
         }
     })
 });
 
-app.listen(8080)
+function createResponse(forecastHeader, forecastText) {
+    return '<p><b>' + forecastHeader + '</b><br/>' + forecastText + '</p>';
+}
 
-console.log('Web scraping server started on port 8080');
+function sendTweet(forecastHeader, forecastText) {
+    // TODO: Send tweet to correct Twitter account
+}
+
+app.listen(8080)
 
 exports = module.exports = app;
