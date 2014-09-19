6de87595bc253672974fce9e54ebc43ae0ffd5a5
Per Jansson <per.r.jansson@gmail.com>
2014-09-19 19:52:14 +0200

    Added cloudno env. variable to use as port.

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ function sendTweet(areaKey, forecast) {
     forecastTweeter.tweet(areaKey, forecast);
 }
 
-var port = Number(process.env.PORT || 1337);
+var port = Number(process.env.app_port || process.env.PORT || 1337);
 app.listen(port)
 
 exports = module.exports = app;
