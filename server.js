6fb868f67741577e35c9cd2763bb614e2fb65295
Per Jansson <per.r.jansson@gmail.com>
2014-09-21 10:50:13 +0200

    Changed cron job to 5 min again.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const TWEET_TIME_FORMAT_PATTERN = "HH:mm";
  * Cron job
  */
 try {
-    new cronJob('*/15 * * * *', function () {
+    new cronJob('*/5 * * * *', function () {
         forecastProvider.getLastUpdatedTime(function (error, lastUpdatedTime) {
             if (!error) {
                 if (lastUpdatedTime.isAfter(lastTweetTime)) {
@@ -35,7 +35,7 @@ try {
             }
         });
     }, null, true);
-    log("Cron job started, ready to run every 15 minutes.");
+    log("Cron job started, ready to run every 5 minutes.");
 } catch (ex) {
     log("Cron pattern not valid");
 }
