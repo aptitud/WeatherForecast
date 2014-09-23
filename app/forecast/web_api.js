7c1fc4d703f49da28f1b4d680c8726d1e5307288
Per Jansson <per.r.jansson@gmail.com>
2014-09-23 22:53:33 +0200

    Returning error codes and created an error page.

--- app/forecast/web_api.js
+++ app/forecast/web_api.js
@@ -16,7 +16,7 @@ function startWebApi() {
             if (!error) {
                 createAndSendResponse(res, forecasts);
             } else {
-                createAndSendErrorResponse(res, error);
+                createAndSendErrorResponse(res, 500, "Oops! That didn't go as planned: " + error);
             }
         });
     })
@@ -25,15 +25,15 @@ function startWebApi() {
         var areaKey = req.params.area;
         var areaName = forecastAreaMapper.mapForecastKeyToName(areaKey);
         if (typeof areaName === 'undefined') {
-            var errorMessage = "Could not find area name for area key: " + areaKey;
+            var errorMessage = "Forecast for area key " + areaKey + " could not be found";
             logger.warn(errorMessage);
-            createAndSendErrorResponse(res, errorMessage);
+            createAndSendErrorResponse(res, 404, errorMessage);
         } else {
             forecastRepository.get(areaName, function (error, forecast) {
                 if (!error) {
                     createAndSendResponse(res, [forecast]);
                 } else {
-                    createAndSendErrorResponse(res, error);
+                    createAndSendErrorResponse(res, 500, "Oops! That didn't go as planned: " + error);
                 }
             });
         }
@@ -47,17 +47,21 @@ function startWebApi() {
 
 function createAndSendResponse(res, forecasts) {
     createResponse(forecasts, function (response) {
-        res.send(response);
+        res.status(200).send(response);
     });
 }
 
-function createAndSendErrorResponse(res, error) {
-    res.send("Oops! That didn't go as planned:<br/>" + error);
+function createAndSendErrorResponse(res, errorCode, errorMessage) {
+    var response = "<html>" + createHead() + "<body>";
+    response = response.concat("<p class='error'>");
+    response = response.concat("<div class='error-code'>" + errorCode + "</div>");
+    response = response.concat("<div class='error-message'>" + errorMessage + "</div>");
+    response = response.concat("</p></body></html>");
+    res.status(errorCode).send(response);
 }
 
 function createResponse(forecasts, callback) {
-    var css = fs.readFileSync("app/forecast/style.css", "utf8");
-    var response = "<html><head><meta charset='utf-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'><title>Sjöväderprognos från SMHI</title><link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'><link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Amatic+SC'><style>" + css + "</style></head><body>";
+    var response = "<html>" + createHead() + "<body>";
     for (var i = 0; i < forecasts.length; i++) {
         response = response.concat("<p class='forecast'><a href='/Sjovaderprognos/" + forecasts[i].areaKey + "'><span class='area-name'>" + forecasts[i].areaName + "</span> kl: <span class='forecast-time'>" + forecasts[i].time + "</span><br/><span class='forecast-text'>\"" + forecasts[i].forecast + "\"</span></a></p>")
     }
@@ -72,6 +76,11 @@ function createResponse(forecasts, callback) {
     } else {
         callback(response.concat("</p></body></html>"));
     }
+}
+
+function createHead() {
+    var css = fs.readFileSync("app/forecast/style.css", "utf8");
+    return "<head><meta charset='utf-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'><title>Sjöväderprognos från SMHI</title><link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'><link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Amatic+SC'><style>" + css + "</style></head>";
 }
 
 module.exports.startWebApi = startWebApi;
